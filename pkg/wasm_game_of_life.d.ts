573202f9fe4ce0fb2b31bae2c8e914b522167c43
Theo <theo.gianella@proton.me>
2023-07-01 23:54:12 +0200

    Adding pkg

---
+++ pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead = 0,
+  Alive = 1,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @param {boolean} blank
+* @returns {Universe}
+*/
+  static new(blank: boolean): Universe;
+/**
+* @param {number} width
+*/
+  set_width(width: number): void;
+/**
+* @param {number} height
+*/
+  set_height(height: number): void;
+/**
+* @returns {string}
+*/
+  render(): string;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+/**
+* @param {number} row
+* @param {number} column
+*/
+  toggle_cell(row: number, column: number): void;
+}
