0a9bd4533e530e89daab8dcb861b3f3d2ff029c6
Theo <theo.gianella@proton.me>
2023-07-02 00:05:58 +0200

    Updating index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Universe, Cell } from "wasm-game-of-life";
-import { memory } from "wasm_game_of_life/wasm_game_of_life_bg";
+import { memory } from "./pkg/wasm_game_of_life_bg";
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
