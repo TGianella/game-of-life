4e188c739b0d0f08dbe25fab43e6d47300cafabf
Theo <theo.gianella@proton.me>
2023-12-31 13:36:37 +0100

    try to change universe construction for wasm

--- createUniverse.js
+++ createUniverse.js
@@ -1,6 +1,6 @@
 import { Universe as UniverseWasm } from "wasm-game-of-life";
 import { Universe as UniverseJs } from "./pkg/game-of-life.js";
 
-export const createUniverseWasm = (blank, width, height) => UniverseWasm.new(blank, width, height);
+export const createUniverseWasm = (blank, width, height) => new UniverseWasm(blank, width, height);
 
 export const createUniverseJs = (blank, width, height) => new UniverseJs(blank, width, height);
